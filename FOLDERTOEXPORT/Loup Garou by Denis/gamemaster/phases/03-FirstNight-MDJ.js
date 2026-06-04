ac5c8efb80e373be4f46eeedcbc81cfe1d52453f
Denis <soaresden@gmail.com>
2026-06-04 18:03:19 +0200

    chore: nettoyage fichiers legacy + changelogs

--- FOLDERTOEXPORT/Loup Garou by Denis/gamemaster/phases/03-FirstNight-MDJ.js
+++
@@ -1,2 +0,0 @@
-/* DEPRECATED: ce fichier a ete decoupe en 03-FirstNightMDJ-00-core.js ... -06-ui-timer-styles.js.
-   Il n'est plus charge par index.html. Conserve uniquement pour reference: voir .prebak */
